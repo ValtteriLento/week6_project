6b7a05b6276c9d0268b2a45022d658d6c0ae896c
ValtteriLento <valtteri.lento@gmail.com>
2022-10-13 11:23:46 +0000

    Update 5 files

--- dist/src.a2b27638.js
+++ dist/src.a2b27638.js
@@ -229,7 +229,7 @@ function initializeCode() {
                 data: chartData,
                 type: "line",
                 height: 450,
-                color: "#eb5146"
+                colors: ["#eb5146"]
               });
             case 13:
             case "end":
@@ -269,7 +269,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "37551" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "35997" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
